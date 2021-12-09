9b854fc692abf6babc37caa74aaefcabc7b57304
Bhavesh <bhic2030@gmail.com>
2021-12-09 20:18:41 +0530

    adding our vision

---
+++ client/src/components/ourVision/OurVision.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./OurVision.css"
+
+const OurVision = () => {
+  return (
+    <div className="our-vision-container">
+      <div className="heading">
+        <h1>Our Vision</h1>
+      </div>
+      <div className="content">
+        <p>
+          We aim at encouraging people to turn creative ideas which they present
+          into innovative products and services,and instill entrepreneurial
+          spirit and willingness to move forward to advance their idea.
+        </p>
+        <p>
+          How one can brainstorm creatively beyond the basic whiteboard
+          scribbling.
+        </p>
+        <p>
+          And create an entrepreneurial community who provide each other
+          pathways to reach each others goals by setting missions and vison in
+          their lives
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default OurVision;
--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import OurVision from '../../components/ourVision/OurVision'
 
 const Home = () => {
     return (
         <div>
-            <h1>Home</h1>
+            <OurVision></OurVision>
         </div>
     )
 }
